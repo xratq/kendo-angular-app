d36ee1ed03efc6a70f53d2431d1f80a0c90c42cf
Agowert1 <104188722+Agowert1@users.noreply.github.com>
2022-10-04 10:39:19 +0300

    graavgvsAD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,38 @@
 import { Component } from '@angular/core';
 import { Product } from "./app.module"
+import { Customers } from "./customers";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <kendo-grid
+      [kendoGridBinding]="gridData"
+      [pageSize]="10"
+      [pageable]="true"
+      [sortable]="true"
+      [filterable]="true"
+      [groupable]="true"
+      [height]="420"
+    >
+      <kendo-grid-column field="CompanyName" [width]="140"></kendo-grid-column>
+      <kendo-grid-column field="ContactName" [width]="120"></kendo-grid-column>
+      <kendo-grid-column field="City" [width]="100"></kendo-grid-column>
+      <kendo-grid-column field="ContactTitle" [width]="130"></kendo-grid-column>
+    </kendo-grid>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'kendo-angular-app';
-  public gridData: Product[] = [
-    {
-        ProductID: 1,
-        ProductName: 'Chai',
-        UnitPrice: 18,
-        Category: {
-            CategoryID: 1,
-            CategoryName: 'Beverages'
-        }
-    },
-];
+  public gridData: unknown[] = Customers;
+  // public gridData: Product[] = [
+  //   {
+  //       ProductID: 1,
+  //       ProductName: 'Chai',
+  //       UnitPrice: 18,
+  //       Category: {
+  //           CategoryID: 1,
+  //           CategoryName: 'Beverages'
+  //       }
+// ];
 }
